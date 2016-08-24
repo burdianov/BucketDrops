fefef5ca2c7bc991ae39ca904c9d8db11178fda9
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-24 21:01:01 +0400

    Implement Button onClickListener

--- app/src/main/java/com/testography/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/testography/bucketdrops/ActivityMain.java
@@ -3,13 +3,24 @@ package com.testography.bucketdrops;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolbar;
+    Button mBtnAdd;
+
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(ActivityMain.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,9 @@ public class ActivityMain extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         initBackgroundImage();
+
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
     }
 
     private void initBackgroundImage() {
