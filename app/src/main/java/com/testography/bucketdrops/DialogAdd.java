3fe2e81876456bdb6a1b85da0e692069ae31b1b9
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-24 23:00:08 +0400

    Launch the fragment dialog from ActivityMain

---
+++ app/src/main/java/com/testography/bucketdrops/DialogAdd.java
@@ -0,0 +1,20 @@
+package com.testography.bucketdrops;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogAdd extends DialogFragment {
+
+    public DialogAdd() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add, container, false);
+    }
+}
