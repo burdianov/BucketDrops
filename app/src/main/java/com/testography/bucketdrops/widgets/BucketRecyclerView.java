037a09f58ec457d802295dcc3527708d6aabe5c7
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-27 13:34:44 +0400

    Add an instance of AdapterDataObserver

--- app/src/main/java/com/testography/bucketdrops/widgets/BucketRecyclerView.java
+++ app/src/main/java/com/testography/bucketdrops/widgets/BucketRecyclerView.java
@@ -7,6 +7,38 @@ import android.util.AttributeSet;
 
 public class BucketRecyclerView extends RecyclerView {
 
+    private AdapterDataObserver mObserver = new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            super.onItemRangeChanged(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount, Object payload) {
+            super.onItemRangeChanged(positionStart, itemCount, payload);
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            super.onItemRangeMoved(fromPosition, toPosition, itemCount);
+        }
+    };
+
     public BucketRecyclerView(Context context) {
         super(context);
     }
