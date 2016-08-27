885c3236f1a08409fb56d893d98c2c9e0a52eb13
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-27 13:38:54 +0400

    Add setAdapter method

--- app/src/main/java/com/testography/bucketdrops/widgets/BucketRecyclerView.java
+++ app/src/main/java/com/testography/bucketdrops/widgets/BucketRecyclerView.java
@@ -10,32 +10,32 @@ public class BucketRecyclerView extends RecyclerView {
     private AdapterDataObserver mObserver = new AdapterDataObserver() {
         @Override
         public void onChanged() {
-            super.onChanged();
+
         }
 
         @Override
         public void onItemRangeChanged(int positionStart, int itemCount) {
-            super.onItemRangeChanged(positionStart, itemCount);
+
         }
 
         @Override
         public void onItemRangeChanged(int positionStart, int itemCount, Object payload) {
-            super.onItemRangeChanged(positionStart, itemCount, payload);
+
         }
 
         @Override
         public void onItemRangeInserted(int positionStart, int itemCount) {
-            super.onItemRangeInserted(positionStart, itemCount);
+
         }
 
         @Override
         public void onItemRangeRemoved(int positionStart, int itemCount) {
-            super.onItemRangeRemoved(positionStart, itemCount);
+
         }
 
         @Override
         public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
-            super.onItemRangeMoved(fromPosition, toPosition, itemCount);
+
         }
     };
 
@@ -49,5 +49,14 @@ public class BucketRecyclerView extends RecyclerView {
 
     public BucketRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        super.setAdapter(adapter);
+        if (adapter != null) {
+            adapter.registerAdapterDataObserver(mObserver);
+        }
+        mObserver.onChanged();
     }
 }
