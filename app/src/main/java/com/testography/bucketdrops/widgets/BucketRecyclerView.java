ab1ad2a3f64d4fdfa8c5e0345d6ffa6ff0d4851b
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-27 13:33:44 +0400

    Add a subclass of RecyclerView

---
+++ app/src/main/java/com/testography/bucketdrops/widgets/BucketRecyclerView.java
@@ -0,0 +1,21 @@
+package com.testography.bucketdrops.widgets;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+public class BucketRecyclerView extends RecyclerView {
+
+    public BucketRecyclerView(Context context) {
+        super(context);
+    }
+
+    public BucketRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BucketRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
